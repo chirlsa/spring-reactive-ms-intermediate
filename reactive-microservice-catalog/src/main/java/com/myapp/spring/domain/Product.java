9fe57e71576ec30bf69cc6daf76a8807b38f08e4
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2022-11-28 16:09:37 +0530

    product repo

--- reactive-microservice-catalog/src/main/java/com/myapp/spring/domain/Product.java
+++ reactive-microservice-catalog/src/main/java/com/myapp/spring/domain/Product.java
@@ -1,8 +1,18 @@
 package com.myapp.spring.domain;
 
+import java.util.Set;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("ecomproducts")
 public record Product(
 		
-		String id
-		) {
+		@PrimaryKey ProductKey key,
+		String description,
+		Set<String> images) {
+	public static Product build(String id,String name,Double price,String description,Set<String> images) {
+		return new Product(new ProductKey(id,name,price), description, images);
+	}
 
 }
