8ecb476450836660475c7027e2fcdf88b4314ff3
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2022-11-28 16:17:32 +0530

    product service updated

--- reactive-microservice-catalog/src/main/java/com/myapp/spring/repository/ProductRepository.java
+++ reactive-microservice-catalog/src/main/java/com/myapp/spring/repository/ProductRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.cassandra.repository.ReactiveCassandraRepository
 import com.myapp.spring.domain.Product;
 import com.myapp.spring.domain.ProductKey;
 
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveCassandraRepository<Product, ProductKey> {
 	
 	@Query("select * from reactive.ecomproducts where product_id=?0 and product_name=?1")
-	Flux<Product> findByIdAndName(String id,String name);
+	Mono<Product> findByIdAndName(String id,String name);
 
 }
