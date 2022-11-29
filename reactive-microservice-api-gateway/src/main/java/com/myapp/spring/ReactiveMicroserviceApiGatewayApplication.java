c0e41588e42b61ed5ef9a06d0dd3c25cf4dc31ad
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2022-11-29 15:16:35 +0530

    cloud api gateway completed without redis

--- reactive-microservice-api-gateway/src/main/java/com/myapp/spring/ReactiveMicroserviceApiGatewayApplication.java
+++ reactive-microservice-api-gateway/src/main/java/com/myapp/spring/ReactiveMicroserviceApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.myapp.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReactiveMicroserviceApiGatewayApplication {
 
 	public static void main(String[] args) {
