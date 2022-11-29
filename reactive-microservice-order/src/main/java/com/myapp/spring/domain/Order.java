7ba1aa550fdc88299397a2fc3074f40e800c3f12
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2022-11-29 10:07:38 +0530

    order microservice

---
+++ reactive-microservice-order/src/main/java/com/myapp/spring/domain/Order.java
@@ -0,0 +1,30 @@
+package com.myapp.spring.domain;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("ecomorders")
+public record Order(
+		
+		@PrimaryKey
+		Long id,
+		String productId,
+		String productName,
+		Double price,
+		Integer quantity,
+		@CreatedDate
+		Instant createdDate,
+		@LastModifiedDate
+		Instant lastModifiedDate
+		
+		
+		) {
+	public static Order of(String productId,String productName,Double productPrice,Integer quantity) {
+		return new Order(null,productId,productName,productPrice,quantity,null,null);
+	}
+
+}
